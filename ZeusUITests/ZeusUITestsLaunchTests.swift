d70228eb26adfae449137491ff805c9fc41718a1
Priyanshu Verma <priyanshuverma1238@gmail.com>
2022-12-29 14:55:06 +0530

    Initial Commit

---
+++ ZeusUITests/ZeusUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ZeusUITestsLaunchTests.swift
+//  ZeusUITests
+//
+//  Created by Priyanshu Verma on 29/12/22.
+//
+
+import XCTest
+
+final class ZeusUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
