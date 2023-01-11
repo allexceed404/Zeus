f60925e4c053738b4a133c20ee0745688316b029
Priyanshu Verma <priyanshuverma1238@gmail.com>
2023-01-11 11:33:49 +0530

    Removed SceneDelegate, migrated app initialisation to AppDelegate

--- Zeus/AppDelegate.swift
+++ Zeus/AppDelegate.swift
@@ -11,52 +11,22 @@ import GoogleMaps
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var windowScene: UIWindowScene?
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-//        print(String(localized: "top_headlines"))
         GMSServices.provideAPIKey(Secrets.GMSapiKey.rawValue)
         
-        if #available(iOS 13.0, *) {
-
-        }
-        else {
-            window = UIWindow(frame: UIScreen.main.bounds)
-            window?.rootViewController = TabBarController()
-            window?.makeKeyAndVisible()
-        }
+        NetworkMonitor.shared.startMonitoring()
         
-//        window = UIWindow(frame: UIScreen.main.bounds)
-//        window?.windowScene = windowScene
-//        print(UIScreen.main.bounds)
-//
-////        NetworkMonitor.shared.newtorkStatusHandlerDelegate = self
-//        NetworkMonitor.shared.startMonitoring()
-//
-//        let navigationController = NavigationController(rootViewController: TabBarController())
-//        navigationController.navigationBar.isHidden = true
-//        window?.rootViewController = navigationController
-//
-//        window?.makeKeyAndVisible()
+        window = UIWindow(frame: UIScreen.main.bounds)
+        
+        let navigationController = NavigationController(rootViewController: TabBarController())
+        navigationController.navigationBar.isHidden = true
+        window?.rootViewController = navigationController
+
+        window?.makeKeyAndVisible()
         
         return true
-    }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-//        NetworkMonitor.shared.stopMonitoring()
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 }
 
